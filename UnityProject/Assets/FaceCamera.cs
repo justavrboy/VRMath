89c352f7749b83128dc7dd7bb23b5a68f66cb2ae
VR Boy <justavrboy@gmail.com>
2016-11-05 21:17:47 -0700

    First commit with grid texture, Steam VR

---
+++ UnityProject/Assets/FaceCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+    public TextMesh textMesh;
+
+	// Use this for initialization
+	void Start() {
+       textMesh = GetComponent<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update() {
+       textMesh.transform.LookAt(Camera.main.transform.position);
+       textMesh.text = "Text";
+    }
+}
