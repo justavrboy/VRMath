975c7ffc038a805a0b75e33474d0d8a485dbc96c
VR Boy <justavrboy@gmail.com>
2016-11-05 21:37:38 -0700

    GridManager places grids floored position relative to controller

--- UnityProject/Assets/GridManager.cs
+++ UnityProject/Assets/GridManager.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class GridManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject VRController;
+    [SerializeField]
+    private GameObject GuidanceGrid;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +14,13 @@ public class GridManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        GuidanceGrid.transform.position = FloorVector(VRController.transform.position);
 	}
+
+    private Vector3 FloorVector(Vector3 originalPos) {
+        float x = (Mathf.Floor(originalPos.x));
+        float y = (Mathf.Floor(originalPos.y));
+        float z = (Mathf.Floor(originalPos.z));
+        return new Vector3(x, y, z);
+    }
 }
